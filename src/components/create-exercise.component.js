525f910d0fa1ff22551ffbc10762a5de39306265
Maria Palacios <imarip09@gmail.com>
2019-09-24 15:52:26 -0400

    Added starter components & fixed the edit exercise comp file name

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CreateExercise extends Component {
+  
+  render() {
+    return (
+      <div>
+        <p>You are on the ECreate Exercise Component!</p>
+      </div>
+    )
+  }
+}
