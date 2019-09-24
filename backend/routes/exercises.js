e77627a2c315850c3127dd6c9cda0a2986fcf7d6
Maria Palacios <imarip09@gmail.com>
2019-09-24 14:25:23 -0400

    created exercises routes

---
+++ backend/routes/exercises.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+let Exercise = require('../models/user.model');
+
+//Routes inside our route files read as '/exercises/____' because of how we set up our routes in the server file.
+//The one below would be '/exercises/
+router.route('/').get((req, res) => {
+  Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+//This one would be '/exercises/add
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = Date.parse(req.body.date);
+
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date
+  });
+
+  newExercise.save()
+    .then(() => res.json('Exercise added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+module.exports = router;
